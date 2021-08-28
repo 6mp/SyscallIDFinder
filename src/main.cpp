9fae2dcb81ae889648d42d18d498b649cc569d9a
matt <30198937+mp7878@users.noreply.github.com>
2021-08-28 15:33:21 -0400

    small tweak

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ int main( ) {
 	Io::InitConsole( );
 	std::string module_name{ };
 	std::string function_name{ };
+	std::string lower_function_name{ };
 
 	Io::Log( "enter syscall module, ex ntdll or ntdll.dll\n" );
 	std::cin >> module_name;
@@ -12,25 +13,25 @@ int main( ) {
 	Io::Log( "enter syscall name, ex NtCreateThread\n" );
 	std::cin >> function_name;
 
-	//tolower fn name
-	std::ranges::transform( function_name.begin( ), function_name.end( ), function_name.begin( ),
-							[]( std::uint8_t c ) -> std::uint8_t { return std::tolower( c ); } );
+	// tolower fn name
+	std::transform( function_name.cbegin( ), function_name.cend( ), std::back_inserter( lower_function_name ),
+					[]( std::uint8_t c ) -> std::uint8_t { return std::tolower( c ); } );
 
-	//fixup name if someone only typed name without extension
+	// fixup name if someone only typed name without extension
 	if ( module_name.find( '.' ) == std::string::npos )
 		module_name += ".dll";
 
 	ExportParser module( reinterpret_cast<std::uintptr_t>( LoadLibraryA( module_name.c_str( ) ) ) );
 
-	if ( !module.GetFunctions( ).contains( function_name ) ) {
+	if ( !module.GetFunctions( ).contains( lower_function_name ) ) {
 		Io::Log<fmt::color::red>( "function {} was not found in module {}\n", function_name, module_name );
 		return 0;
 	}
 
 	Io::Log<fmt::color::green>( "{} id is {:X}\n", function_name,
-								GetSyscallIndex( module.GetFunctions( )[ function_name ] ) );
+								GetSyscallIndex( module.GetFunctions( )[ lower_function_name ] ) );
 
-	/* doing this method results in 6 invalid syscalls, I am looking for a better way to sort them
+	/* doing this method results in 5 invalid syscalls, I am looking for a better way to filter them
 	ExportParser ntdll( reinterpret_cast<std::uintptr_t>( LoadLibrary( "ntdll.dll" ) ) );
 	ntdll.EnumerateExports( []( std::string_view name, std::uintptr_t address ) -> void {
 		if ( name.substr( 0, 2 ).find( "Nt" ) != std::string::npos ) {
