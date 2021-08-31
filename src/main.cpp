00516e9155ac09b7ec524aae5a35e46137e28bcf
matt <30198937+mp7878@users.noreply.github.com>
2021-08-31 15:56:05 -0400

    final

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int main( ) {
 	std::string function_name{ };
 	std::string lower_function_name{ };
 
-	Io::Log( "enter syscall module, ex ntdll or ntdll.dll\n" );
+	Io::Log( "enter syscall module, ex ntdll.dll\n" );
 	std::cin >> module_name;
 
 	Io::Log( "enter syscall name, ex NtCreateThread\n" );
@@ -30,13 +30,4 @@ int main( ) {
 
 	Io::Log<fmt::color::green>( "{} id is {:X}\n", function_name,
 								GetSyscallIndex( module.GetFunctions( )[ lower_function_name ] ) );
-
-	/* doing this method results in 5 invalid syscalls, I am looking for a better way to filter them
-	ExportParser ntdll( reinterpret_cast<std::uintptr_t>( LoadLibrary( "ntdll.dll" ) ) );
-	ntdll.EnumerateExports( []( std::string_view name, std::uintptr_t address ) -> void {
-		if ( name.substr( 0, 2 ).find( "Nt" ) != std::string::npos ) {
-			Io::Log( "name {}, address {:X}, index {:X}\n", name, address, GetSyscallIndex( address ) );
-		}
-	} );
-	*/
 }
